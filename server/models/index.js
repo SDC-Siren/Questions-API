df64dc4bc66c1372a97f9fe140f65cde769e45f3
pdaly91 <patrickdaly91@gmail.com>
2023-07-14 10:55:48 -0700

    add answers and photos to questions response

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,27 @@ const db = require('../db');
 module.exports.getQuestions = async (product_id, page, count) => {
   try {
     const offset = (page - 1) * count;
-    let result = await db.query(`SELECT * FROM questions WHERE (reported = 'f' AND product_id = '${product_id}') LIMIT ${count} OFFSET ${offset}`);
+    let result = await db.query(
+      `SELECT question_id, question_body, question_date, asker_name, question_helpfulness, reported FROM questions
+        WHERE (reported = 'f' AND product_id = '${product_id}')
+        LIMIT ${count} OFFSET ${offset}`
+    );
+    await Promise.all(result.rows.map(async (question) => {
+      let answers = await db.query(
+        `SELECT answer_id AS id, answer_body AS body, answer_date AS date, answerer_name, answer_helpfulness AS helpfulness FROM answers
+        WHERE (reported = 'f' AND question_id = '${question.question_id}')
+        LIMIT 10`
+      )
+      question.answers = {};
+      await Promise.all(answers.rows.map(async (answer) => {
+        let photos = await db.query(
+          `SELECT url FROM photos WHERE answer_id = '${answer.id}'`
+        )
+        answer.photos = photos.rows;
+        question.answers[answer.id] = answer;
+      }));
+    }));
+    await result.rows;
     let response = {
       "product_id": product_id,
       "results": result.rows
@@ -17,7 +37,11 @@ module.exports.getQuestions = async (product_id, page, count) => {
 module.exports.getAnswers = async (question_id, page, count) => {
   try {
     const offset = (page - 1) * count;
-    let result = await db.query(`SELECT * FROM answers WHERE (reported = 'f' AND question_id = '${question_id}') LIMIT ${count} OFFSET ${offset}`);
+    let result = await db.query(
+      `SELECT answer_id, answer_body AS body, answer_date AS date, answerer_name, answer_helpfulness AS helpfulness FROM answers
+        WHERE (reported = 'f' AND question_id = '${question_id}')
+        LIMIT ${count} OFFSET ${offset}`
+      );
     let response = {
       "question": question_id,
       "page": page,
