1c52d789896c0453b3a146ca1a18969adf31a5be
pdaly91 <patrickdaly91@gmail.com>
2023-07-18 14:48:02 -0700

    update db connection

--- server/db/index.js
+++ server/db/index.js
@@ -5,6 +5,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
+  password: process.env.DB_PW,
   port: process.env.DB_PORT
 });
 
